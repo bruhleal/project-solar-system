be77474219d84477336d96a88f4fcf3a3b80d8f8
bruhleal <bruna11leal@live.com>
2022-10-21 15:29:12 -0400

    req 5 ok

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p>{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = ({
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+}).isRequired;
+
+export default PlanetCard;
