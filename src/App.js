6f8d7d86e8fc099c3264899260d8776ced52b1f4
bruhleal <bruna11leal@live.com>
2022-10-21 17:07:10 -0400

    comentando app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import MissionCard from './components/MissionCard';
+// import MissionCard from './components/MissionCard';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
@@ -11,7 +11,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
-        <MissionCard />
+        {/* <MissionCard /> */}
       </div>
     );
   }
